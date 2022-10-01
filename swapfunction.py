1eae5ca5a895cb287ce10b257dc1e2ec8696c5da
GauravPandey7656 <gauravpandey7656@gmail.com>
2022-10-01 16:36:57 +0530

    swapfunction.py

--- swapfunction.py
+++ swapfunction.py
@@ -1,11 +1,14 @@
-from re import X
+from re import A
 
 
 x=5 
 y=10
+a=x
+b=y
 temp=x
 x=y
 y=temp
-
-print("Value of x is:-",x)
-print("Value of y is:-",y)
+print("Value of x Before Swap is:-",a)
+print("Value of y Before Swap is:-",b)
+print("Value of x After Swap is:-",x)
+print("Value of y After SWap is:-",y)
