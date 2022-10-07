93d9593712d2a004037601606709c2977798fc72
GauravPandey7656 <gauravpandey7656@gmail.com>
2022-10-07 21:02:40 +0530

    square.py

---
+++ square.py
@@ -0,0 +1,12 @@
+a=int(input("Enter a number :-"))
+b=input("what do You want:- ")
+if (b=="square"):
+    sq = a*a
+    print("Square of the given number is:- ",sq)
+elif (b=="cube"):
+    cube= a*a*a
+    print("Cube of the given numbers:- ",cube)
+elif (b=="power"):
+    po=int(input("How many times :-"))
+    pow=a**po
+    print("Power of the number is :-",pow)
