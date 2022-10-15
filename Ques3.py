dcdb774e06727977516b5f3174751c702bab8763
Gaurav Pandey <gauravpandey7656@gmail.com>
2022-10-15 13:36:00 +0530

    Ques3.py

---
+++ Ques3.py
@@ -0,0 +1,12 @@
+x=int(input())
+if x<200:
+    print("Var is less than 200")
+    if x<150:
+        print("Var is less than 150")
+    elif x==150:
+        print("it is 150")
+    elif x<10:
+        print("Var is than 10")
+else:
+    print("not Valid")
+print("Hello World")
