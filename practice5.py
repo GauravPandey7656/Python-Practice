2a1709c8de0af21bca8880c683f3ac2963aed7c6
Gaurav Pandey <gauravpandey7656@gmail.com>
2022-11-12 09:30:23 +0530

    practice5.py

---
+++ practice5.py
@@ -0,0 +1,6 @@
+#write a program to create a function using following conditions 
+
+#it should accept employee name and salary and display both 
+#if the salary is missing then assign the default value as 10000 to salary 
+
+#ben(12000) #mike(15000) #bob()
